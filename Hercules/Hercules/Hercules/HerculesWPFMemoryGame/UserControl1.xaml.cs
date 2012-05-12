d5fec108b6b7171284ce34781f9d00f26726027f
gosha1128 <george.williams@gmail.com>
2012-05-12 01:10:29 -0400

    stuff

--- Hercules/Hercules/Hercules/HerculesWPFMemoryGame/UserControl1.xaml.cs
+++ Hercules/Hercules/Hercules/HerculesWPFMemoryGame/UserControl1.xaml.cs
@@ -158,7 +158,7 @@ namespace HerculesWPFMemoryGame
             {
                 
                 this.pause.Tick += new EventHandler(this.__timeout);
-                this.pause.Interval = new TimeSpan(0, 0, 5);
+                this.pause.Interval = new TimeSpan(0, 0, 2);
                 this.pause.Stop();
             }
 
@@ -274,7 +274,7 @@ namespace HerculesWPFMemoryGame
 
         void write(String w)
         {
-            return;
+            //return;
 
             System.IO.StreamWriter wr = new System.IO.StreamWriter("c:\\tmp\\log.txt", true);
 
