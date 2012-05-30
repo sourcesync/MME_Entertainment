c87aea85c94fc06f196a179f6eddef85a93b0f9b
C George Williams <george.williams@gmail.com>
2012-05-30 16:25:54 -0400

    stuff

---
+++ Hercules/Hercules/Hercules/TestPlatformInvoke/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace TestPlatformInvoke
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public static extern int MessageBox(int hWnd, String text,
+                            String caption, uint type);
+    
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MessageBox(0, "yo", "yo", 0);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
