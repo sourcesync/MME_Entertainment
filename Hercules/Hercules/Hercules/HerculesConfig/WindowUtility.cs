3e0bae6b657ddcd3ec8d66f77ad956dad256274e
unknown <mocap@vicon3_win7.(none)>
2012-06-07 17:50:47 -0400

    stuff

--- Hercules/Hercules/Hercules/HerculesConfig/WindowUtility.cs
+++ Hercules/Hercules/Hercules/HerculesConfig/WindowUtility.cs
@@ -153,6 +153,36 @@ namespace MME.HerculesConfig
             }
         }
 
+        /*
+        public static System.Collections.Hashtable GetMenuDescription()
+        {
+            System.Collections.Hashtable hash = new System.Collections.Hashtable();
+
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                Path = string.Format("Skins\\{0}\\Menu", ConfigUtility.Skin)
+                using (StreamReader sr = new StreamReader("TestFile.txt"))
+                {
+                    String line;
+                    // Read and display lines from the file until the end of
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+         * */
+
         public static System.Collections.ArrayList GetMenuIconFilenames()
         {
             System.Collections.ArrayList result = new System.Collections.ArrayList();
