ebaff5edfd04668326c583a31b92d83d7df3febc
George Williams <george.williams@gmail.com>
2013-08-10 14:08:43 -0700

    stuff

--- Hercules/EOS/Test/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ Hercules/EOS/Test/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -23,17 +23,37 @@ namespace WindowsFormsApplication1
 
             uint i = EDSDKLib.EDSDK.EdsInitializeSDK();
             System.Windows.Forms.MessageBox.Show("Init SDK status=" + i.ToString());
+            if (i == 0)
+            {
+                IntPtr camlist = IntPtr.Zero;
+                i = EDSDKLib.EDSDK.EdsGetCameraList(out camlist);
+                System.Windows.Forms.MessageBox.Show("Get Camera List status=" + i.ToString());
+                if (i == 0)
+                {
+                    int count = 0;
+                    i = EDSDKLib.EDSDK.EdsGetChildCount(camlist, out count);
+                    System.Windows.Forms.MessageBox.Show("Get Camera Count status=" + i.ToString() + " count=" + count.ToString());
+                    if ((i == 0) && (count > 0))
+                    {
 
-            IntPtr camlist = IntPtr.Zero;
-            i = EDSDKLib.EDSDK.EdsGetCameraList(out camlist);
-            System.Windows.Forms.MessageBox.Show("Get Camera List status=" + i.ToString());
+                        IntPtr cam = IntPtr.Zero;
+                        i = EDSDKLib.EDSDK.EdsGetChildAtIndex(camlist, 0, out cam);
+                        if (i == 0)
+                        {
+                            System.Windows.Forms.MessageBox.Show("Get Child at 0 status=" + i.ToString() + " about to take pic!");
 
-            int count = 0;
-            i = EDSDKLib.EDSDK.EdsGetChildCount(camlist, out count);
-            System.Windows.Forms.MessageBox.Show("Get Camera Count status=" + i.ToString() + " count=" + count.ToString());
+                            i = EDSDKLib.EDSDK.EdsSendCommand(cam, EDSDKLib.EDSDK.CameraCommand_TakePicture, 0);
+                            //  kkEdsCameraCommand_TakePicture , 0);
 
-            i = EDSDKLib.EDSDK.EdsTerminateSDK();
-            System.Windows.Forms.MessageBox.Show("Terminate SDK status=" + i.ToString());
+                            System.Windows.Forms.MessageBox.Show("After Take Pic status=" + i.ToString());
+                        }
+                    }
+                }
+
+                i = EDSDKLib.EDSDK.EdsTerminateSDK();
+                System.Windows.Forms.MessageBox.Show("Terminate SDK status=" + i.ToString());
+            }
+                
         }
     }
 }
